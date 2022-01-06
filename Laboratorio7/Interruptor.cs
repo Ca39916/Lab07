8cf60b9db289db25fe38ca753e0811cfab14bb69
Camila Rezende <csalinor@emeal.nttdata.com>
2022-01-06 10:52:49 -0300

    ExercicioLab07

---
+++ Laboratorio7/Interruptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    internal class Interruptor : IEstadoBinario //Criação de classe binaria com implementação da interface IEstadoBinario
+    {
+        private bool ligado;
+
+        public void Ligar()
+        {
+            ligado = true;
+        }
+        public void Desligar()
+        {
+            ligado = false;
+        }
+                
+        public EstadoBinario Estado //informante para o Program
+        {
+            get
+            {
+                if (ligado)
+                    return EstadoBinario.Ligado;
+                else
+                    return EstadoBinario.Desligado;
+            }
+        }
+
+
+    }
+}
---
+++ Laboratorio7/TermometroEletrico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    public class TermometroEletrico : Termometro, IEstadoBinario
+    {
+        private bool ligado = false;
+        public void Ligar()
+        {
+            ligado = true;
+        }
+        public void Desligar()
+        {
+            ligado = false;
+        }
+        public EstadoBinario Estado
+        {
+            get
+            {
+                if (ligado)
+                    return EstadoBinario.Ligado;
+                else
+                    return EstadoBinario.Desligado;
+            }
+        }
+    }
+}
